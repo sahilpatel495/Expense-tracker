de59ca6040b302b133126674e2a17080a50aeb8d
sahilpatel495 <sahilpatel495@gmail.com>
2023-03-01 19:24:07 +0530

    static completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -24,21 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/Components/Expenses/Expenses.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
